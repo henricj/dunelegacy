a4a2dd98d198711e914c3b1fe9bc1bd1ba05e4f7
Richard Schaller <richard.schaller@gmx.de>
2014-06-22 18:32:27 +0200

    Bugfix: Bullets accidentally hitting allies could start a shoot-out

--- src/players/AIPlayer.cpp
+++ src/players/AIPlayer.cpp
@@ -112,7 +112,7 @@ void AIPlayer::onDecrementStructures(int itemID, const Coord& location) {
 void AIPlayer::onDamage(const ObjectBase* pObject, int damage, Uint32 damagerID) {
     const ObjectBase* pDamager = getObject(damagerID);
 
-    if(pDamager == NULL || pDamager->getOwner() == getHouse()) {
+    if(pDamager == NULL || pDamager->getOwner()->getTeam() == getHouse()->getTeam()) {
         return;
     }
 
