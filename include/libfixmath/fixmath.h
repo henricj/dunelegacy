2f99f8f36006b626a1023f5e92e48b0061550a9a
Richard Schaller <richard.schaller@gmx.de>
2016-01-21 11:45:02 +0100

    Add libfixmath to project

---
+++ include/libfixmath/fixmath.h
@@ -0,0 +1,21 @@
+#ifndef __libfixmath_fixmath_h__
+#define __libfixmath_fixmath_h__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*!
+	\file fixmath.h
+	\brief Functions to perform fast accurate fixed-point math operations.
+*/
+
+#include "int64.h"
+#include "fix16.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
