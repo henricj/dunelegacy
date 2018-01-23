60962b5ced19ce693f6e2a798d1c7be51c8fadee
Henric Jungheim <software@henric.org>
2018-01-23 11:24:06 -0700

    Use currentGameMap->for_all() and ->for_each() when drawing. And some clang-tidy...

--- src/Game.cpp
+++ src/Game.cpp
@@ -201,13 +201,13 @@ void Game::initReplay(const std::string& filename) {
 void Game::processObjects()
 {
     // update all tiles
-    for(int y = 0; y < currentGameMap->getSizeY(); y++) {
-        for(int x = 0; x < currentGameMap->getSizeX(); x++) {
+    for(auto y = 0; y < currentGameMap->getSizeY(); y++) {
+        for(auto x = 0; x < currentGameMap->getSizeX(); x++) {
             currentGameMap->getTile(x,y)->update();
         }
     }
 
-    for(StructureBase* pStructure : structureList) {
+    for(auto pStructure : structureList) {
         pStructure->update();
     }
 
@@ -215,15 +215,15 @@ void Game::processObjects()
         currentCursorMode = CursorMode_Normal;
     }
 
-    for(UnitBase* pUnit : unitList) {
+    for(auto pUnit : unitList) {
         pUnit->update();
     }
 
-    for(Bullet* pBullet : bulletList) {
+    for(auto pBullet : bulletList) {
         pBullet->update();
     }
 
-    for(Explosion* pExplosion : explosionList) {
+    for(auto pExplosion : explosionList) {
         pExplosion->update();
     }
 }
@@ -231,14 +231,21 @@ void Game::processObjects()
 
 void Game::drawScreen()
 {
-    Coord TopLeftTile = screenborder->getTopLeftTile();
-    Coord BottomRightTile = screenborder->getBottomRightTile();
+    auto TopLeftTile = screenborder->getTopLeftTile();
+    auto BottomRightTile = screenborder->getBottomRightTile();
 
     // extend the view a little bit to avoid graphical glitches
     TopLeftTile.x = std::max(0, TopLeftTile.x - 1);
     TopLeftTile.y = std::max(0, TopLeftTile.y - 1);
-    BottomRightTile.x = std::min(currentGameMap->getSizeX()-1, BottomRightTile.x + 1);
-    BottomRightTile.y = std::min(currentGameMap->getSizeY()-1, BottomRightTile.y + 1);
+    BottomRightTile.x = std::min(currentGameMap->getSizeX() - 1, BottomRightTile.x + 1);
+    BottomRightTile.y = std::min(currentGameMap->getSizeY() - 1, BottomRightTile.y + 1);
+
+    const auto x1 = TopLeftTile.x;
+    const auto y1 = TopLeftTile.y;
+    const auto x2 = BottomRightTile.x + 1;
+    const auto y2 = BottomRightTile.y + 1;
+
+    const auto tiles = currentGameMap;
 
     const auto x1 = TopLeftTile.x;
     const auto y1 = TopLeftTile.y;
@@ -289,13 +296,13 @@ void Game::drawScreen()
         });
 
     /* draw bullets */
-    for(const Bullet* pBullet : bulletList) {
+    for (const auto pBullet : bulletList) {
         pBullet->blitToScreen();
     }
 
 
     /* draw explosions */
-    for(const Explosion* pExplosion : explosionList) {
+    for (const auto pExplosion : explosionList) {
         pExplosion->blitToScreen();
     }
 
@@ -307,9 +314,9 @@ void Game::drawScreen()
         });
 
     // draw the gathering point line if a structure is selected
-    if(selectedList.size() == 1) {
-        StructureBase *pStructure = dynamic_cast<StructureBase*>(getObjectManager().getObject(*selectedList.begin()));
-        if(pStructure != nullptr) {
+    if (selectedList.size() == 1) {
+        const auto pStructure = dynamic_cast<StructureBase*>(getObjectManager().getObject(*selectedList.begin()));
+        if (pStructure != nullptr) {
             pStructure->drawGatheringPointLine();
         }
     }
@@ -330,14 +337,17 @@ void Game::drawScreen()
         SDL_Texture* hiddenTexZoomed = pGFXManager->getZoomedObjPic(ObjPic_Terrain_Hidden, currentZoomlevel);
         SDL_Texture* hiddenFogTexZoomed = pGFXManager->getZoomedObjPic(ObjPic_Terrain_HiddenFog, currentZoomlevel);
         int zoomedTileSize = world2zoomedWorld(TILESIZE);
-        for(int x = screenborder->getTopLeftTile().x - 1; x <= screenborder->getBottomRightTile().x + 1; x++) {
-            for (int y = screenborder->getTopLeftTile().y - 1; y <= screenborder->getBottomRightTile().y + 1; y++) {
 
-                if((x >= 0) && (x < currentGameMap->getSizeX()) && (y >= 0) && (y < currentGameMap->getSizeY())) {
-                    Tile* pTile = currentGameMap->getTile(x, y);
+        const auto fogOfWar = gameInitSettings.getGameOptions().fogOfWar;
+        const auto zoomedTileSize = world2zoomedWorld(TILESIZE);
+        const auto hideTile = pTile->getHideTile(pLocalHouse->getTeamID());
 
-                    if(pTile->isExploredByTeam(pLocalHouse->getTeamID())) {
-                        int hideTile = pTile->getHideTile(pLocalHouse->getTeamID());
+        tiles->for_each(x1, y1, x2, y2,
+            [=](Tile& t) {
+                const auto x = t.getLocation().x;
+                const auto y = t.getLocation().y;
+
+                 if(pTile->isExploredByTeam(pLocalHouse->getTeamID())) {
 
                         if(hideTile != 0) {
                             SDL_Rect source = { hideTile*zoomedTileSize, 0, zoomedTileSize, zoomedTileSize };
@@ -356,8 +366,8 @@ void Game::drawScreen()
                             if(fogTile != 0) {
                                 SDL_Rect source = { fogTile*zoomedTileSize, 0,
                                                     zoomedTileSize, zoomedTileSize };
-                                SDL_Rect drawLocation = {   screenborder->world2screenX(x*TILESIZE), screenborder->world2screenY(y*TILESIZE),
-                                                            zoomedTileSize, zoomedTileSize };
+                        SDL_RenderCopy(renderer, hiddenTex, &source, &drawLocation);
+                    }
 
                                 SDL_RenderCopy(renderer, hiddenFogTexZoomed, &source, &drawLocation);
                             }
@@ -377,6 +387,30 @@ void Game::drawScreen()
                                                 zoomedTileSize, zoomedTileSize };
                     SDL_RenderCopy(renderer, hiddenTexZoomed, &source, &drawLocation);
                 }
+                else {
+                    if (!debug) {
+                        SDL_Rect source = { zoomedTileSize * 15, 0, zoomedTileSize, zoomedTileSize };
+                        SDL_Rect drawLocation = { screenborder->world2screenX(x*TILESIZE), screenborder->world2screenY(y*TILESIZE),
+                                                    zoomedTileSize, zoomedTileSize };
+                        SDL_RenderCopy(renderer, hiddenTex, &source, &drawLocation);
+                    }
+                }
+            });
+
+        const auto size_x = std::min(tiles->getSizeX(), screenborder->getBottomRightTile().x + 1);
+        const auto size_y = std::min(tiles->getSizeY(), screenborder->getBottomRightTile().y + 1);
+        for (auto x = screenborder->getTopLeftTile().x - 1; x <= screenborder->getBottomRightTile().x + 1; ++x) {
+            for (auto y = screenborder->getTopLeftTile().y - 1; y <= screenborder->getBottomRightTile().y + 1; ++y) {
+                if (x >= 0 && x < size_x)
+                    x = size_x;
+                if (y >= 0 && y < size_y)
+                    y = size_y;
+
+                // we are outside the map => draw complete hidden
+                SDL_Rect source = { zoomedTileSize * 15, 0, zoomedTileSize, zoomedTileSize };
+                SDL_Rect drawLocation = { screenborder->world2screenX(x*TILESIZE), screenborder->world2screenY(y*TILESIZE),
+                    zoomedTileSize, zoomedTileSize };
+                SDL_RenderCopy(renderer, hiddenTex, &source, &drawLocation);
             }
         }
     }
@@ -389,8 +423,8 @@ void Game::drawScreen()
         if(screenborder->isScreenCoordInsideMap(drawnMouseX, drawnMouseY)) {
             //if mouse is not over game bar
 
-            int xPos = screenborder->screen2MapX(drawnMouseX);
-            int yPos = screenborder->screen2MapY(drawnMouseY);
+            const int xPos = screenborder->screen2MapX(drawnMouseX);
+            const int yPos = screenborder->screen2MapY(drawnMouseY);
 
             if(selectedList.size() == 1) {
                 auto pBuilder = dynamic_cast<BuilderBase*>(objectManager.getObject(*selectedList.begin()));
@@ -1235,7 +1269,10 @@ void Game::runMainLoop() {
             if(gameCycleCount <= skipToGameCycle) {
                 frameTime = 0;
             } else {
-                frameTime -= getGameSpeed();
+                if (frameTime > 200)
+                    frameTime = 0;
+                else
+                    frameTime -= getGameSpeed();
             }
         }
 
@@ -1444,8 +1481,8 @@ bool Game::loadSaveGame(InputStream& stream) {
     }
 
     //read map size
-    short mapSizeX = stream.readUint32();
-    short mapSizeY = stream.readUint32();
+    const short mapSizeX = stream.readUint32();
+    const short mapSizeY = stream.readUint32();
 
     //create the new map
     currentGameMap = new Map(mapSizeX, mapSizeY);
@@ -1462,7 +1499,7 @@ bool Game::loadSaveGame(InputStream& stream) {
     objectData.load(stream);
 
     //load the house(s) info
-    for(int i=0; i<NUM_HOUSES; i++) {
+    for(auto i=0; i<NUM_HOUSES; i++) {
         if (stream.readBool() == true) {
             //house in game
             house[i] = std::make_unique<House>(stream);
@@ -1472,7 +1509,7 @@ bool Game::loadSaveGame(InputStream& stream) {
     // we have to set the local player
     if(bMultiplayerLoad) {
         // get it from the gameInitSettings that started the game (not the one saved in the savegame)
-        for(const GameInitSettings::HouseInfo& houseInfo : oldHouseInfoList) {
+        for(const auto& houseInfo : oldHouseInfoList) {
 
             // find the right house
             for(int i=0;i<NUM_HOUSES;i++) {
@@ -1509,7 +1546,7 @@ bool Game::loadSaveGame(InputStream& stream) {
         }
     } else {
         // it is stored in the savegame, so set it up
-        Uint8 localPlayerID = stream.readUint8();
+        const auto localPlayerID = stream.readUint8();
         pLocalPlayer = dynamic_cast<HumanPlayer*>(getPlayerByID(localPlayerID));
         pLocalHouse = house[pLocalPlayer->getHouse()->getHouseID()].get();
     }
@@ -1666,13 +1703,11 @@ void Game::saveObject(OutputStream& stream, ObjectBase* obj) const
 }
 
 
-ObjectBase* Game::loadObject(InputStream& stream, Uint32 objectID)
+ObjectBase* Game::loadObject(InputStream& stream, Uint32 objectID) const
 {
-    Uint32 itemID;
+    const auto itemID = stream.readUint32();
 
-    itemID = stream.readUint32();
-
-    ObjectBase* newObject = ObjectBase::loadObject(stream, itemID, objectID);
+    const auto newObject = ObjectBase::loadObject(stream, itemID, objectID);
     if(newObject == nullptr) {
         THROW(std::runtime_error, "Error while loading an object!");
     }
@@ -1681,24 +1716,24 @@ ObjectBase* Game::loadObject(InputStream& stream, Uint32 objectID)
 }
 
 
-void Game::selectAll(const std::set<Uint32>& aList)
+void Game::selectAll(const Dune::selected_set_type& aList) const
 {
-    for(Uint32 objectID : aList) {
+    for(auto objectID : aList) {
         objectManager.getObject(objectID)->setSelected(true);
     }
 }
 
 
-void Game::unselectAll(const std::set<Uint32>& aList)
+void Game::unselectAll(const Dune::selected_set_type& aList) const
 {
-    for(Uint32 objectID : aList) {
+    for(auto objectID : aList) {
         objectManager.getObject(objectID)->setSelected(false);
     }
 }
 
-void Game::onReceiveSelectionList(const std::string& name, const std::set<Uint32>& newSelectionList, int groupListIndex)
+void Game::onReceiveSelectionList(const std::string& name, const Dune::selected_set_type& newSelectionList, int groupListIndex)
 {
-    HumanPlayer* pHumanPlayer = dynamic_cast<HumanPlayer*>(getPlayerByName(name));
+    auto pHumanPlayer = dynamic_cast<HumanPlayer*>(getPlayerByName(name));
 
     if(pHumanPlayer == nullptr) {
         return;
@@ -1711,8 +1746,8 @@ void Game::onReceiveSelectionList(const std::string& name, const std::set<Uint32
             return;
         }
 
-        for(Uint32 objectID : selectedByOtherPlayerList) {
-            ObjectBase* pObject = objectManager.getObject(objectID);
+        for(auto objectID : selectedByOtherPlayerList) {
+            auto pObject = objectManager.getObject(objectID);
             if(pObject != nullptr) {
                 pObject->setSelectedByOtherPlayer(false);
             }
@@ -1732,7 +1767,7 @@ void Game::onReceiveSelectionList(const std::string& name, const std::set<Uint32
     }
 }
 
-void Game::onPeerDisconnected(const std::string& name, bool bHost, int cause) {
+void Game::onPeerDisconnected(const std::string& name, bool bHost, int cause) const {
     pInterface->getChatManager().addInfoMessage(name + " disconnected!");
 }
 
@@ -1816,12 +1851,12 @@ void Game::handleChatInput(SDL_KeyboardEvent& keyboardEvent) {
         if(typingChatMessage.length() > 0) {
             unsigned char md5sum[16];
 
-            md5((const unsigned char*) typingChatMessage.c_str(), typingChatMessage.size(), md5sum);
+            md5(reinterpret_cast<const unsigned char*>(typingChatMessage.c_str()), typingChatMessage.size(), md5sum);
 
             std::stringstream md5stream;
             md5stream << std::setfill('0') << std::hex << std::uppercase << "0x";
-            for(int i=0;i<16;i++) {
-                md5stream << std::setw(2) << (int) md5sum[i];
+            for(int i : md5sum) {
+                md5stream << std::setw(2) << i;
             }
 
             const std::string md5string = md5stream.str();
@@ -1909,20 +1944,20 @@ void Game::handleKeyInput(SDL_KeyboardEvent& keyboardEvent) {
         case SDLK_8:
         case SDLK_9: {
             //for SDLK_1 to SDLK_9 select group with that number, if ctrl create group from selected obj
-            int selectListIndex = keyboardEvent.keysym.sym - SDLK_1;
+            const auto selectListIndex = keyboardEvent.keysym.sym - SDLK_1;
 
             if(SDL_GetModState() & KMOD_CTRL) {
                 pLocalPlayer->setGroupList(selectListIndex, selectedList);
 
                 pInterface->updateObjectInterface();
             } else {
-                std::set<Uint32>& groupList = pLocalPlayer->getGroupList(selectListIndex);
+                auto& groupList = pLocalPlayer->getGroupList(selectListIndex);
 
                 // find out if we are choosing a group with all items already selected
                 bool bEverythingWasSelected = (selectedList.size() == groupList.size());
                 Coord averagePosition;
-                for(Uint32 objectID : groupList) {
-                    ObjectBase* pObject = objectManager.getObject(objectID);
+                for(auto objectID : groupList) {
+                    auto pObject = objectManager.getObject(objectID);
                     bEverythingWasSelected = bEverythingWasSelected && pObject->isSelected();
                     averagePosition += pObject->getLocation();
                 }
@@ -1942,8 +1977,8 @@ void Game::handleKeyInput(SDL_KeyboardEvent& keyboardEvent) {
                 }
 
                 // now we add the selected items
-                for(Uint32 objectID : groupList) {
-                    ObjectBase* pObject = objectManager.getObject(objectID);
+                for(auto objectID : groupList) {
+                    auto pObject = objectManager.getObject(objectID);
                     if(pObject->getOwner() == pLocalHouse) {
                         pObject->setSelected(true);
                         selectedList.insert(pObject->getObjectID());
@@ -2023,21 +2058,21 @@ void Game::handleKeyInput(SDL_KeyboardEvent& keyboardEvent) {
         } break;
 
         case SDLK_F1: {
-            Coord oldCenterCoord = screenborder->getCurrentCenter();
+            const auto oldCenterCoord = screenborder->getCurrentCenter();
             currentZoomlevel = 0;
             screenborder->adjustScreenBorderToMapsize(currentGameMap->getSizeX(), currentGameMap->getSizeY());
             screenborder->setNewScreenCenter(oldCenterCoord);
         } break;
 
         case SDLK_F2: {
-            Coord oldCenterCoord = screenborder->getCurrentCenter();
+            const auto oldCenterCoord = screenborder->getCurrentCenter();
             currentZoomlevel = 1;
             screenborder->adjustScreenBorderToMapsize(currentGameMap->getSizeX(), currentGameMap->getSizeY());
             screenborder->setNewScreenCenter(oldCenterCoord);
         } break;
 
         case SDLK_F3: {
-            Coord oldCenterCoord = screenborder->getCurrentCenter();
+            const auto oldCenterCoord = screenborder->getCurrentCenter();
             currentZoomlevel = 2;
             screenborder->adjustScreenBorderToMapsize(currentGameMap->getSizeX(), currentGameMap->getSizeY());
             screenborder->setNewScreenCenter(oldCenterCoord);
@@ -2079,8 +2114,8 @@ void Game::handleKeyInput(SDL_KeyboardEvent& keyboardEvent) {
         case SDLK_m: {
             //set object to move
             if(currentCursorMode != CursorMode_Move) {
-                for(Uint32 objectID : selectedList) {
-                    ObjectBase* pObject = objectManager.getObject(objectID);
+                for(const auto objectID : selectedList) {
+                    const auto pObject = objectManager.getObject(objectID);
                     if(pObject->isAUnit() && (pObject->getOwner() == pLocalHouse) && pObject->isRespondable()) {
                         currentCursorMode = CursorMode_Move;
                         break;
@@ -2091,14 +2126,14 @@ void Game::handleKeyInput(SDL_KeyboardEvent& keyboardEvent) {
 
         case SDLK_g: {
             // select next construction yard
-            std::set<Uint32> itemIDs;
+            Dune::selected_set_type itemIDs;
             itemIDs.insert(Structure_ConstructionYard);
             selectNextStructureOfType(itemIDs);
         } break;
 
         case SDLK_f: {
             // select next factory
-            std::set<Uint32> itemIDs;
+            Dune::selected_set_type itemIDs;
             itemIDs.insert(Structure_Barracks);
             itemIDs.insert(Structure_WOR);
             itemIDs.insert(Structure_LightFactory);
@@ -2467,7 +2502,7 @@ void Game::selectNextStructureOfType(const Dune::selected_set_type& itemIDs) {
     bool bSelectNext = true;
 
     if(selectedList.size() == 1) {
-        ObjectBase* pObject = getObjectManager().getObject(*selectedList.begin());
+        const auto pObject = getObjectManager().getObject(*selectedList.begin());
         if((pObject != nullptr) && (itemIDs.count(pObject->getItemID()) == 1)) {
             bSelectNext = false;
         }
@@ -2475,7 +2510,7 @@ void Game::selectNextStructureOfType(const Dune::selected_set_type& itemIDs) {
 
     StructureBase* pStructure2Select = nullptr;
 
-    for(StructureBase* pStructure : structureList) {
+    for(const auto pStructure : structureList) {
         if(bSelectNext) {
             if( (itemIDs.count(pStructure->getItemID()) == 1) && (pStructure->getOwner() == pLocalHouse) ) {
                 pStructure2Select = pStructure;
@@ -2490,7 +2525,7 @@ void Game::selectNextStructureOfType(const Dune::selected_set_type& itemIDs) {
 
     if(pStructure2Select == nullptr) {
         // start over at the beginning
-        for(StructureBase* pStructure : structureList) {
+        for(auto pStructure : structureList) {
             if( (itemIDs.count(pStructure->getItemID()) == 1) && (pStructure->getOwner() == pLocalHouse) && !pStructure->isSelected() ) {
                 pStructure2Select = pStructure;
                 break;
