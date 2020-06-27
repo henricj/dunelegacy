db203d1fbdaebf68560646a2debd31cb09aa780a
Henric Jungheim <software@henric.org>
2020-06-27 00:58:15 -0700

    Add a POSIX vcpkg build script.

---
+++ IDE/VC/vcpkg/build_vcpkg.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+Packages="sdl2 sdl2-mixer sdl2-ttf fmt ms-gsl"
+
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+
+"$dir/vcpkg/bootstrap-vcpkg.sh"
+
+"$dir/vcpkg/vcpkg" install --triplet x64-linux $Packages
+
