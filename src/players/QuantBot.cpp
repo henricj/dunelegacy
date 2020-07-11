071ca89bcdcd245a8653e5c5dd978614be326dea
Henric Jungheim <software@henric.org>
2020-07-11 04:22:55 -0700

    QuantBot: static_cast -> dune_cast

--- src/players/QuantBot.cpp
+++ src/players/QuantBot.cpp
@@ -976,8 +976,7 @@ void QuantBot::build(int militaryValue) {
 
             // End of unit ratio optimisation algorithm
 
-            if(pStructure->isABuilder()) {
-                const auto* pBuilder = static_cast<const BuilderBase*>(pStructure);
+            if(const auto* const pBuilder = dune_cast<BuilderBase>(pStructure)) {
                 switch (pStructure->getItemID()) {
 
                     case Structure_LightFactory: {
