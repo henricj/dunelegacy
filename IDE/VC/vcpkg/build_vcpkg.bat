275715ab6997cd9e0387484e34ae991052ba15e3
Henric Jungheim <software@henric.org>
2020-07-19 10:26:53 -0700

    Add GitHub workflows for Windows and Ubuntu.

--- IDE/VC/vcpkg/build_vcpkg.bat
+++ IDE/VC/vcpkg/build_vcpkg.bat
@@ -2,10 +2,17 @@ SET Packages=sdl2 sdl2-mixer sdl2-ttf fmt ms-gsl gtest
 
 copy "%~dp0triplets\*.*" "%~dp0vcpkg\triplets\"
 
-call "%~dp0vcpkg\bootstrap-vcpkg.bat"
+if NOT EXIST "%~dp0vcpkg\vcpkg.exe" call "%~dp0vcpkg\bootstrap-vcpkg.bat"
 
+if [%1] == [] GOTO buildAll
+
+"%~dp0vcpkg\vcpkg.exe" install %* %Packages%
+
+exit 0
+
+buildAll:
 "%~dp0vcpkg\vcpkg.exe" install --triplet x64-windows %Packages%
 "%~dp0vcpkg\vcpkg.exe" install --triplet x64-windows-ltcg %Packages%
 "%~dp0vcpkg\vcpkg.exe" install --triplet x86-windows %Packages%
 "%~dp0vcpkg\vcpkg.exe" install --triplet x86-windows-ltcg %Packages%
-
+"%~dp0vcpkg\vcpkg.exe" install --triplet arm64-windows-ltcg %Packages%
