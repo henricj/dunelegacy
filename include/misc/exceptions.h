6060faab3f10933a4fdeed5bd320923e8f7030a1
Henric Jungheim <software@henric.org>
2020-07-21 04:17:21 -0700

    Make sure THROW() has at least one argument.

--- include/misc/exceptions.h
+++ include/misc/exceptions.h
@@ -23,7 +23,14 @@
 #include <fmt/format.h>
 #include <fmt/printf.h>
 
-#define THROW(TException, ...) throw TException(fmt::sprintf("%s:%d: %s", __FILE__, __LINE__, fmt::sprintf(__VA_ARGS__)))
+template<typename TException, typename... Args>
+TException dune_exception(std::string_view file, int line, std::string_view format, Args&&... args) {
+    static_assert(std::is_base_of<std::exception, TException>::value);
+
+    return TException{fmt::sprintf("%s:%d: %s", file, line, fmt::sprintf(format, std::forward<Args>(args)...))};
+}
+
+#define THROW(TException, ...) throw dune_exception<TException>(__FILE__, __LINE__, __VA_ARGS__)
 
 class io_error : public std::runtime_error {
     using std::runtime_error::runtime_error;
