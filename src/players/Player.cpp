b1fb47d96149e8d5e189d95a510f4a9583448e28
Henric Jungheim <software@henric.org>
2020-08-03 03:06:17 -0700

    Prefer setState() to creating a new Random instance.

--- src/players/Player.cpp
+++ src/players/Player.cpp
@@ -51,7 +51,7 @@ Player::Player(const GameContext& context, InputStream& stream, House* associate
     playername = stream.readString();
     const auto state  = stream.readUint8Vector();
     if(state.size() != decltype(random_)::state_bytes) THROW(std::runtime_error, "Random state size mismatch!");
-    random_ = Random::create(state);
+    random_.setState(state);
 }
 
 Player::~Player() {
