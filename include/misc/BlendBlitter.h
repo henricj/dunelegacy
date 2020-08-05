1fb28578642130c86858596034259f7d112c9d3c
Henric Jungheim <software@henric.org>
2020-08-04 20:42:58 -0700

    Add an implementation file for BlendBlitter.

--- include/misc/BlendBlitter.h
+++ include/misc/BlendBlitter.h
@@ -2,42 +2,11 @@
 #define BLENDBLITTER_H
 
 #include <misc/SDL2pp.h>
-#include <misc/draw_util.h>
-#include <misc/exceptions.h>
-#include <mmath.h>
-
-#include <cstdio>
-#include <cstdlib>
 
 // Step by step one more pixel of the source image is blitted to the destination image
 class BlendBlitter {
 public:
-    BlendBlitter(sdl2::surface_ptr SrcPic, SDL_Surface* DestPic, SDL_Rect DestPicRect, int numSteps = 50) {
-        src = std::move(SrcPic);
-        dest = DestPic;
-        destRect = DestPicRect;
-        this->numSteps = numSteps;
-
-        N = static_cast<Uint64>(src->w)*static_cast<Uint64>(src->h);
-        // compute next greater 2^x value
-        m = N;
-        m |= (m >> 1);
-        m |= (m >> 2);
-        m |= (m >> 4);
-        m |= (m >> 8);
-        m |= (m >> 16);
-        m |= (m >> 32);
-        m++;
-
-        auto& random = pGFXManager->random();
-
-        c = random.rand(0, static_cast<int>((m / 2) - 1)) * 2 + 1;  // c is any odd number from [0;m]
-        a = random.rand(0, static_cast<int>((m / 4) - 1)) * 4 + 1;  // (a-1) is divisible by all prime factors of log_2(m), and 4
-
-        currentValue = random.rand(0, static_cast<int>(m - 1));
-
-        StepsLeft = numSteps;
-    }
+    BlendBlitter(sdl2::surface_ptr SrcPic, SDL_Surface* DestPic, SDL_Rect DestPicRect, int numSteps = 50);
 
     ~BlendBlitter() = default;
 
@@ -53,45 +22,13 @@ public:
         Blits the next pixel to the destination surface
         \return The number of steps to do
     */
-    int nextStep() {
-        if(StepsLeft <= 0) {
-            return 0;
-        }
-        StepsLeft--;
-
-        sdl2::surface_lock lock_dest{ dest };
-        sdl2::surface_lock lock_src{ src.get() };
-
-        Uint64 numPixelsPerStep = (N / numSteps) + 1;
-        for(Uint64 i=0;i<numPixelsPerStep;i++) {
-            Uint64 cur = getNextValue();
-
-            int x = static_cast<int>(cur % src->w);
-            int y = static_cast<int>(cur / src->w);
-
-            Uint32 color = getPixel(src.get(), x, y);
-
-            if(color != 0) {
-
-                if( (destRect.x + x < dest->w) && (destRect.x + x >= 0) &&
-                    (destRect.x + x <= destRect.x + destRect.w) &&
-                    (destRect.y + y < dest->h) && (destRect.y + y >= 0) &&
-                    (destRect.y + y <= destRect.y + destRect.h) ) {
-
-                    // is inside destRect and the destination surface
-                    putPixel(dest, destRect.x + x, destRect.y + y, color);
-                }
-            }
-        }
-
-        return StepsLeft;
-    }
+    int nextStep();
 
 
 private:
     sdl2::surface_ptr src;
     SDL_Surface* dest;
-    SDL_Rect    destRect;
+    SDL_Rect    destRect{};
     int numSteps;
     int StepsLeft;
 
---
+++ src/misc/BlendBlitter.cpp
@@ -0,0 +1,56 @@
+#include <misc/BlendBlitter.h>
+
+BlendBlitter::BlendBlitter(sdl2::surface_ptr SrcPic, SDL_Surface* DestPic, SDL_Rect DestPicRect, int numSteps)
+    : src{std::move(SrcPic)}, dest{DestPic}, destRect{DestPicRect}, numSteps{numSteps} {
+
+    N = static_cast<Uint64>(src->w) * static_cast<Uint64>(src->h);
+
+    // compute next greater 2^x value
+    m = N;
+    m |= (m >> 1);
+    m |= (m >> 2);
+    m |= (m >> 4);
+    m |= (m >> 8);
+    m |= (m >> 16);
+    m |= (m >> 32);
+    m++;
+
+    auto& random = pGFXManager->random();
+
+    c = random.rand(0, static_cast<int>((m / 2) - 1)) * 2 + 1; // c is any odd number from [0;m]
+    a = random.rand(0, static_cast<int>((m / 4) - 1)) * 4 + 1;
+    // (a-1) is divisible by all prime factors of log_2(m), and 4
+
+    currentValue = random.rand(0, static_cast<int>(m - 1));
+
+    StepsLeft = numSteps;
+}
+
+int BlendBlitter::nextStep() {
+    if(StepsLeft <= 0) { return 0; }
+
+    StepsLeft--;
+
+    sdl2::surface_lock lock_dest{dest};
+    sdl2::surface_lock lock_src{src.get()};
+
+    const Uint64 numPixelsPerStep = (N / numSteps) + 1;
+    for(Uint64 i = 0; i < numPixelsPerStep; i++) {
+        const auto cur = getNextValue();
+
+        const auto x = static_cast<int>(cur % src->w);
+        const auto y = static_cast<int>(cur / src->w);
+
+        const Uint32 color = getPixel(src.get(), x, y);
+
+        if(color != 0) {
+            if((destRect.x + x < dest->w) && (destRect.x + x >= 0) && (destRect.x + x <= destRect.x + destRect.w) &&
+               (destRect.y + y < dest->h) && (destRect.y + y >= 0) && (destRect.y + y <= destRect.y + destRect.h)) {
+                // is inside destRect and the destination surface
+                putPixel(dest, destRect.x + x, destRect.y + y, color);
+            }
+        }
+    }
+
+    return StepsLeft;
+}
