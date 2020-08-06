df46dd6e2d2a453b2e7168fd041227377bae9ef7
Henric Jungheim <software@henric.org>
2020-08-06 05:30:11 -0700

    Build vcpkg for x86/avx2 as well.

--- IDE/VC/vcpkg/build_vcpkg.bat
+++ IDE/VC/vcpkg/build_vcpkg.bat
@@ -15,3 +15,4 @@ exit 0
 "%~dp0vcpkg\vcpkg.exe" install --triplet x86-windows-ltcg %Packages%
 "%~dp0vcpkg\vcpkg.exe" install --triplet arm64-windows-ltcg %Packages%
 "%~dp0vcpkg\vcpkg.exe" install --triplet x64-avx2-windows-ltcg %Packages%
+"%~dp0vcpkg\vcpkg.exe" install --triplet x86-avx2-windows-ltcg %Packages%
