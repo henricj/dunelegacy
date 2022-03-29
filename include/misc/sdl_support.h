1cf3e8972a4216001f697f4495223121e8c0bc87
Henric Jungheim <software@henric.org>
2022-03-29 06:05:14 -0700

    Remove redundant "RESTRICT" definition.

--- include/misc/sdl_support.h
+++ include/misc/sdl_support.h
@@ -5,14 +5,3 @@
 namespace sdl2 {
 void SDL_LogRenderer(const SDL_RendererInfo* info);
 }
-
-#ifndef RESTRICT
-#    if _MSC_VER
-#        define RESTRICT __restrict
-#    elif 0
-// TODO: Version check for GCC and clang is needed..?
-#        define RESTRICT __restrict__
-#    else
-#        define RESTRICT
-#    endif
-#endif // RESTRICT
