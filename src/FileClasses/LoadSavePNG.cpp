50fd8f746e41d9080ce519c8adbea52f15f99cbd
Henric Jungheim <software@henric.org>
2022-04-22 16:42:54 -0700

    Move SavePNG()'s implementation out of the header.

--- src/FileClasses/LoadSavePNG.cpp
+++ src/FileClasses/LoadSavePNG.cpp
@@ -200,6 +200,12 @@ int SavePNG_RW(SDL_Surface* surface, SDL_RWops* RWop) {
     return 0;
 }
 
+int SavePNG(SDL_Surface* surface, const std::filesystem::path& file) {
+    const sdl2::RWops_ptr sdl_r_wops{SDL_RWFromFile(file.u8string().c_str(), "wb")};
+
+    return SavePNG_RW(surface, sdl_r_wops.get());
+}
+
 std::tuple<bool, std::optional<std::filesystem::path>> SaveScreenshot() {
     const auto timeinfo0 = dune::dune_localtime();
 
