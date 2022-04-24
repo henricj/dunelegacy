bfaf62a27d9a6f5830fa06f4dc5bb7f0ebf32e24
Henric Jungheim <software@henric.org>
2022-04-24 09:53:54 -0700

    Fix GUIStyle::setLogicalSize()'s fallback path.

--- src/GUI/GUIStyle.cpp
+++ src/GUI/GUIStyle.cpp
@@ -39,8 +39,8 @@ void GUIStyle::setDisplayDpi(float ratio) {
 
 namespace {
 auto scale_xy(float scale, int x, int y) {
-    const auto w = static_cast<int>(std::round(static_cast<float>(x) * scale));
-    const auto h = static_cast<int>(std::round(static_cast<float>(y) * scale));
+    const auto w = static_cast<int>(std::ceil(static_cast<float>(x) * scale));
+    const auto h = static_cast<int>(std::ceil(static_cast<float>(y) * scale));
 
     return std::make_pair(w, h);
 }
@@ -49,19 +49,19 @@ auto scale_xy(float scale, int x, int y) {
 void GUIStyle::setLogicalSize(SDL_Renderer* renderer, int physical_width, int physical_height) {
     auto scale = getScale();
 
-    const auto scale_inverse = 1.f / scale;
+    auto scale_inverse = 1.f / scale;
 
     auto [w, h] = scale_xy(scale_inverse, physical_width, physical_height);
 
     if (w < MINIMUM_WIDTH || h < MINIMUM_HEIGHT) {
-        const auto w_ratio = static_cast<float>(MINIMUM_WIDTH) / static_cast<float>(w);
-        const auto h_ratio = static_cast<float>(MINIMUM_HEIGHT) / static_cast<float>(h);
+        const auto w_ratio = static_cast<float>(physical_width) / static_cast<float>(MINIMUM_WIDTH);
+        const auto h_ratio = static_cast<float>(physical_height) / static_cast<float>(MINIMUM_HEIGHT);
 
-        const auto worst_ratio = std::max(w_ratio, h_ratio);
+        scale = std::min(w_ratio, h_ratio);
 
-        scale /= worst_ratio;
+        scale_inverse = 1.f / scale;
 
-        const auto [w2, h2] = scale_xy(scale_inverse * worst_ratio, physical_width, physical_height);
+        const auto [w2, h2] = scale_xy(scale_inverse, physical_width, physical_height);
 
         w = w2;
         h = h2;
@@ -70,6 +70,8 @@ void GUIStyle::setLogicalSize(SDL_Renderer* renderer, int physical_width, int ph
     actual_scale_ = scale;
 
     SDL_RenderSetLogicalSize(renderer, w, h);
+
+    sdl2::log_info("Setting logical size %dx%d (%dx%d) with scale %f", w, h, physical_width, physical_height, scale);
 }
 
 sdl2::surface_ptr GUIStyle::createEmptySurface(uint32_t width, uint32_t height, bool transparent) {
