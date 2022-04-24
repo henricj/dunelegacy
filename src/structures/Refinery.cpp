25752ff6f132a2ad38b8101ed1e10468deef3e8a
Henric Jungheim <software@henric.org>
2022-04-24 04:43:39 -0700

    Check for "impossible" conditions in Refinery::updateStructureSpecificStuff().

--- src/structures/Refinery.cpp
+++ src/structures/Refinery.cpp
@@ -172,46 +172,66 @@ void Refinery::stopAnimate() {
 }
 
 void Refinery::updateStructureSpecificStuff(const GameContext& context) {
-    if (extractingSpice) {
-        auto* pHarvester = static_cast<Harvester*>(harvester.getObjPointer());
+    if (!extractingSpice)
+        return;
 
-        if (pHarvester->getAmountOfSpice() > 0) {
-            FixPoint extractionSpeed = MAXIMUMHARVESTEREXTRACTSPEED;
+    auto* const unit       = harvester.getObjPointer();
+    auto* const pHarvester = dune_cast<Harvester>(unit);
 
-            int scale = floor(5 * getHealth() / getMaxHealth());
-            if (scale == 0) {
-                scale = 1;
-            }
+    if (!pHarvester) {
+        if (unit) {
+            sdl2::log_error(
+                "A refinery is extracting spice from object %d, but it is type %d, which is not a harvester!",
+                unit->getObjectID(), unit->getItemID());
 
-            extractionSpeed = (extractionSpeed * scale) / 5;
+            deployHarvester(context);
+        } else {
+            sdl2::log_error("A refinery is extracting spice but has no harvester!");
 
-            owner->addCredits(pHarvester->extractSpice(extractionSpeed), true);
-        } else if ((!pHarvester->isAwaitingPickup()) && (pHarvester->getGuardPoint().isValid())) {
-            // find carryall
-            Carryall* pCarryall = nullptr;
-            if ((pHarvester->getGuardPoint().isValid()) && getOwner()->hasCarryalls()) {
-                for (UnitBase* pUnit : unitList) {
-                    if ((pUnit->getOwner() == owner) && (pUnit->getItemID() == Unit_Carryall)) {
-                        auto* pTmpCarryall = static_cast<Carryall*>(pUnit);
-                        if (!pTmpCarryall->isBooked()) {
-                            pCarryall = pTmpCarryall;
-                            break;
-                        }
+            // We don't know how we got here, so leave the bookings alone.
+            extractingSpice = false;
+        }
+
+        return;
+    }
+
+    if (pHarvester->getAmountOfSpice() > 0) {
+        FixPoint extractionSpeed = MAXIMUMHARVESTEREXTRACTSPEED;
+
+        int scale = floor(5 * getHealth() / getMaxHealth());
+        if (scale == 0)
+            scale = 1;
+
+        extractionSpeed = extractionSpeed * scale / 5;
+
+        owner->addCredits(pHarvester->extractSpice(extractionSpeed), true);
+    } else if (!pHarvester->isAwaitingPickup() && pHarvester->getGuardPoint().isValid()) {
+        // find carryall
+        Carryall* pCarryall = nullptr;
+        if (pHarvester->getGuardPoint().isValid() && getOwner()->hasCarryalls()) {
+            for (auto* const pUnit : unitList) {
+                if (pUnit->getOwner() == owner)
+                    continue;
+
+                if (auto* const pTmpCarryall = dune_cast<Carryall>(pUnit)) {
+                    if (!pTmpCarryall->isBooked()) {
+                        pCarryall = pTmpCarryall;
+                        break;
                     }
                 }
             }
+        }
 
-            if (pCarryall != nullptr) {
-                pCarryall->setTarget(this);
-                pCarryall->clearPath();
-                pHarvester->bookCarrier(pCarryall);
-                pHarvester->setTarget(nullptr);
-                pHarvester->setDestination(pHarvester->getGuardPoint());
-            } else {
-                deployHarvester(context);
-            }
-        } else if (!pHarvester->hasBookedCarrier()) {
+        if (pCarryall != nullptr) {
+            pCarryall->setTarget(this);
+            pCarryall->clearPath();
+            pHarvester->bookCarrier(pCarryall);
+            pHarvester->setTarget(nullptr);
+            pHarvester->setDestination(pHarvester->getGuardPoint());
+        } else {
             deployHarvester(context);
         }
+    } else if (!pHarvester->hasBookedCarrier()) {
+        deployHarvester(context);
     }
 }
