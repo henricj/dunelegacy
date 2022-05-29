bccd1e6412299ab3b7163b1b7ea880d05b78759f
Henric Jungheim <software@henric.org>
2022-05-28 18:53:39 -0700

    Provide an adlib.h file that disables some MSVC errors

---
+++ external/adplug/adlib.h
@@ -0,0 +1,16 @@
+#ifndef ADLIB_H
+#define ADLIB_H
+
+#if _MSC_VER
+#    pragma warning(push)
+#    pragma warning(disable : 4100)
+#    pragma warning(disable : 4458)
+#endif // _MSC_VER
+
+#include "adplug/src/adlib.h"
+
+#if _MSC_VER
+#    pragma warning(pop)
+#endif // _MSC_VER
+
+#endif // ADLIB_H
