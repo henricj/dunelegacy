27f2ac2d0bbecc61d0e9306b9139f05bc45f0dd6
Henric Jungheim <software@henric.org>
2022-06-19 10:37:01 -0700

    Don't try to load a map from an empty string.

--- src/INIMap/INIMap.cpp
+++ src/INIMap/INIMap.cpp
@@ -1,12 +1,14 @@
 #include "INIMap/INIMap.h"
 
+#include <gsl/gsl>
+
 INIMap::INIMap(GameType gameType, const std::filesystem::path& mapname, const std::string& mapdata)
     : mapname_(reinterpret_cast<const char*>(mapname.u8string().c_str())) {
     if (gameType == GameType::Campaign || gameType == GameType::Skirmish) {
         // load from PAK-File
         inifile_ = std::make_unique<INIFile>(dune::globals::pFileManager->openFile(this->mapname_).get());
-    } else if (gameType == GameType::CustomGame || gameType == GameType::CustomMultiplayer) {
-        const auto RWops = sdl2::RWops_ptr{SDL_RWFromConstMem(mapdata.c_str(), mapdata.size())};
+    } else if (!mapdata.empty() && (gameType == GameType::CustomGame || gameType == GameType::CustomMultiplayer)) {
+        const auto RWops = sdl2::RWops_ptr{SDL_RWFromConstMem(mapdata.c_str(), gsl::narrow<int>(mapdata.size()))};
 
         inifile_ = std::make_unique<INIFile>(RWops.get());
     } else {
